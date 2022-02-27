dda99f8b746d2f7567c6459122b898f0c0277783
Anton Kerp <anton_kerp@mail.ru>
2022-02-27 16:13:24 +0700

    Добавляем меню Settings
    # Conflicts:
    #	Assets/_Root/Scripts/MainController.cs

--- Assets/_Root/Scripts/Ui/MainMenu/MainMenuController.cs
+++ Assets/_Root/Scripts/Ui/MainMenu/MainMenuController.cs
@@ -7,7 +7,7 @@ namespace Ui
 {
     internal class MainMenuController : BaseController
     {
-        private readonly ResourcePath _resourcePath = new ResourcePath("Prefabs/MainMenu");
+        private readonly ResourcePath _resourcePath = new ResourcePath("Prefabs/Ui/MainMenu");
         private readonly ProfilePlayer _profilePlayer;
         private readonly MainMenuView _view;
 
@@ -16,7 +16,7 @@ namespace Ui
         {
             _profilePlayer = profilePlayer;
             _view = LoadView(placeForUi);
-            _view.Init(StartGame);
+            _view.Init(StartGame, Settings);
         }
 
         private MainMenuView LoadView(Transform placeForUi)
@@ -30,5 +30,8 @@ namespace Ui
 
         private void StartGame() =>
             _profilePlayer.CurrentState.Value = GameState.Game;
+
+        private void Settings() =>
+            _profilePlayer.CurrentState.Value = GameState.Settings;
     }
 }
