94440818985574165348d14c9935f9274c25cec5
Anton Kerp <anton_kerp@mail.ru>
2022-02-28 19:48:24 +0700

    Добавляем ContentDataSourceLoader

---
+++ Assets/_Root/Scripts/Tool/ResourceManagement/ContentDataSourceLoader.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using Features.Shed.Upgrade;
+using Features.AbilitySystem;
+using Features.Inventory.Items;
+
+namespace Tool
+{
+    internal class ContentDataSourceLoader
+    {
+        public static ItemConfig[] LoadItemConfigs(ResourcePath resourcePath)
+        {
+            var dataSource = ResourcesLoader.LoadObject<ItemConfigDataSource>(resourcePath);
+            return dataSource == null ? Array.Empty<ItemConfig>() : dataSource.ItemConfigs.ToArray();
+        }
+    }
+}
