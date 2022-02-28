3ed152c1bb5d112bb342bbf5096cb440a5a3e16d
Anton Kerp <anton_kerp@mail.ru>
2022-02-28 19:48:47 +0700

    Добавляем гараж

--- Assets/_Root/Scripts/Ui/MainMenu/MainMenuView.cs
+++ Assets/_Root/Scripts/Ui/MainMenu/MainMenuView.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 namespace Ui
 {
@@ -8,18 +8,21 @@ namespace Ui
     {
         [SerializeField] private Button _buttonStart;
         [SerializeField] private Button _buttonSettings;
+        [SerializeField] private Button _buttonShed;
 
 
-        public void Init(UnityAction startGame, UnityAction settings)
+        public void Init(UnityAction startGame, UnityAction settings, UnityAction shed)
         {
             _buttonStart.onClick.AddListener(startGame);
             _buttonSettings.onClick.AddListener(settings);
+            _buttonShed.onClick.AddListener(shed);
         }
 
         public void OnDestroy()
         {
             _buttonStart.onClick.RemoveAllListeners();
             _buttonSettings.onClick.RemoveAllListeners();
+            _buttonShed.onClick.RemoveAllListeners();
         }
     }
 }
