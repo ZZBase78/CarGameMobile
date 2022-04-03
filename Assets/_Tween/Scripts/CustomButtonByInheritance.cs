d73ad6ac4ecce9c0aa6a6218de9857ca304153ae
Anton Kerp <anton_kerp@mail.ru>
2022-04-03 20:31:41 +0700

    Добавляем возможность воспроизводить и останавливать твин-анимацию

--- Assets/_Tween/Scripts/CustomButtonByInheritance.cs
+++ Assets/_Tween/Scripts/CustomButtonByInheritance.cs
@@ -19,6 +19,8 @@ namespace Tween
         [SerializeField] private float _duration = 0.6f;
         [SerializeField] private float _strength = 30f;
 
+        private Tweener _tweenAnimation;
+
 
         protected override void Awake()
         {
@@ -42,18 +44,25 @@ namespace Tween
             ActivateAnimation();
         }
 
+        [ContextMenu(nameof(ActivateAnimation))]
         private void ActivateAnimation()
         {
+            StopAnimation();
+
             switch (_animationButtonType)
             {
                 case AnimationButtonType.ChangeRotation:
-                    _rectTransform.DOShakeRotation(_duration, Vector3.forward * _strength).SetEase(_curveEase);
+                    _tweenAnimation = _rectTransform.DOShakeRotation(_duration, Vector3.forward * _strength).SetEase(_curveEase);
                     break;
 
                 case AnimationButtonType.ChangePosition:
-                    _rectTransform.DOShakeAnchorPos(_duration, Vector2.one * _strength).SetEase(_curveEase);
+                    _tweenAnimation = _rectTransform.DOShakeAnchorPos(_duration, Vector2.one * _strength).SetEase(_curveEase);
                     break;
             }
         }
+
+        [ContextMenu(nameof(StopAnimation))]
+        private void StopAnimation() =>
+            _tweenAnimation?.Kill();
     }
 }
