6d8694ce3377145aef520c4d98f3ff0b8de10812
Anton Kerp <anton_kerp@mail.ru>
2022-04-16 10:39:00 +0700

    Добавляем загрузку бандла с новым фоном для кнопки

--- Assets/_Root/Scripts/Tool/Bundles/Examples/AssetBundleViewBase.cs
+++ Assets/_Root/Scripts/Tool/Bundles/Examples/AssetBundleViewBase.cs
@@ -6,15 +6,28 @@ namespace Tool.Bundles.Examples
 {
     internal class AssetBundleViewBase : MonoBehaviour
     {
+        private const string UrlAssetBundleBackgrounds = "https://drive.google.com/uc?export=download&id=1y2gUPCn4mUzfZ6W8L7K4NeNitS03_Pn0";
         private const string UrlAssetBundleSprites = "https://drive.google.com/uc?export=download&id=1rQzWdcChHhJJBTe4rf1D0Kwi1a43jxWR";
         private const string UrlAssetBundleAudio = "https://drive.google.com/uc?export=download&id=1I7euU6Hv5yrn1ektprUumbGHEikklk3Y";
 
+        [SerializeField] private DataSpriteBundle[] _dataBackgroundBundles;
         [SerializeField] private DataSpriteBundle[] _dataSpriteBundles;
         [SerializeField] private DataAudioBundle[] _dataAudioBundles;
 
+        private AssetBundle _backgroundsAssetBundle;
         private AssetBundle _spritesAssetBundle;
         private AssetBundle _audioAssetBundle;
 
+
+        protected IEnumerator DownloadAndSetBackgroundsAssetBundle()
+        {
+            yield return GetBackgroundsAssetBundle();
+
+            if (_backgroundsAssetBundle != null)
+                SetBackgroundAssets(_backgroundsAssetBundle);
+            else
+                Debug.LogError($"AssetBundle {nameof(_backgroundsAssetBundle)} failed to load");
+        }
 
         protected IEnumerator DownloadAndSetSpritesAssetBundles()
         {
@@ -34,6 +47,18 @@ namespace Tool.Bundles.Examples
                 SetAudioAssets(_audioAssetBundle);
             else
                 Debug.LogError($"AssetBundle {nameof(_audioAssetBundle)} failed to load");
+        }
+
+        private IEnumerator GetBackgroundsAssetBundle()
+        {
+            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(UrlAssetBundleBackgrounds);
+
+            yield return request.SendWebRequest();
+
+            while (!request.isDone)
+                yield return null;
+
+            StateRequest(request, out _backgroundsAssetBundle);
         }
 
         private IEnumerator GetSpritesAssetBundle()
@@ -72,6 +97,12 @@ namespace Tool.Bundles.Examples
                 assetBundle = null;
                 Debug.LogError(request.error);
             }
+        }
+
+        private void SetBackgroundAssets(AssetBundle assetBundle)
+        {
+            foreach (DataSpriteBundle data in _dataBackgroundBundles)
+                data.Image.sprite = assetBundle.LoadAsset<Sprite>(data.NameAssetBundle);
         }
 
         private void SetSpriteAssets(AssetBundle assetBundle)
